4faaad318a06c06ea527bdde9cd4b2bd07f67812
Nikita <wolf24ru@gmail.com>
2021-06-29 20:14:17 +0700

    reuest complit

--- Requests(hw8)/reuest.py
+++ Requests(hw8)/reuest.py
@@ -4,30 +4,27 @@ import requests
 def intelligence(super_hero_name):
     search_url = 'https://superheroapi.com/api/2619421814940190/search/'
     result_requests = requests.get(search_url + super_hero_name)
-    num = 1
-    super_dict = {}
+    hero_json = result_requests.json()
+    if hero_json.get('response') == 'success':
 
-    if result_requests.json().get('response') == 'success':
-        hero_json = result_requests.json()
-        for x in hero_json.get('results'):
-
-#---------------- !!!переписать под опредления четого имени!!!!----------
-
-            print(f'{num} {x.get("name")}')
-            super_dict.update({num: x.get("id")})
-            num += 1
-        # num_hero = input('\nВыберите необходимого супер героя:')
-        num_hero = 1
-        for i in hero_json.get('results'):
-            if i.get('id') == super_dict.get(num_hero):
-                return(int(i.get('powerstats').get('intelligence')))
+        for hero in hero_json.get('results'):
+            if hero.get('name') == super_hero_name:
+                return int(hero.get('powerstats').get('intelligence'))
+            else:
+                print('Error 404\nThe hero was not found')
+                return 0
     else:
         print(f'Герой {super_hero_name} не найден')
         return 0
 
 
 if __name__ == '__main__':
-    super_heros_names = ['Hulk', 'Captain America', 'Thanos']
-    # for x in super_heros_names:
-    #     intelligence(x)
-    p = intelligence('Hulk')
+    super_heros = {'Hulk': {}, 'Captain America': {}, 'Thanos': {}}
+
+    for name in super_heros:
+        super_heros[name].update({'intelligence': intelligence(name)})
+
+    the_smartest_hero = sorted(super_heros.items(),
+                               key=lambda smart: smart[1].get('intelligence'),
+                               reverse=True)
+    print(f'Самый умный герой: {the_smartest_hero[0][0]}')
