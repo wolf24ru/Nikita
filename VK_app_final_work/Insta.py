a36e29dfec05043eb52eb87c52b47a922a11a1ef
Nikita <wolf24ru@gmail.com>
2021-07-09 23:34:11 +0700

    update

--- VK_app_final_work/Insta.py
+++ VK_app_final_work/Insta.py
@@ -3,22 +3,7 @@ import requests
 
 class Insta_API:
     def __init__(self, code):
-    	self.token = code
-        # params = {
-        #     'client_id': 1475772392761405,
-        #     'client_secret': '987eea46cea06f67452e7c611fb8fb3f',
-        #     'grant_type': 'authorization_code',
-        #     'redirect_uri': 'https://socialsizzle.herokuapp.com/auth/',
-        #     'code': code
-        # }
-        # r = requests.post(
-        #     'https://api.instagram.com/oauth/access_token', params)
-        # print(r)
-        # if r.status_code == 200:
-        #     self.token = r.json()['access_token']
-        # else:
-        #     print('Ошибка получения токена')
-        #     self.token = 0
+        self.token = code
 
     def get_photo(self, limit):
         params = {
