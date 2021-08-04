0f8f6dafccf3b860b5785a7cafe9141af64cf5ae
Nikita <wolf24ru@gmail.com>
2021-08-04 21:05:54 +0700

    update create_table

--- BD/HW2/CREATE_TABLE_0.sql
+++ BD/HW2/CREATE_TABLE_0.sql
@@ -1,9 +1,9 @@
-create table Style(
+CREATE TABLE Style(
  id serial primary key,
  style_name varchar(60) not null unique
 );
  
-create table Performers(
+CREATE TABLE Performers(
 	id serial primary key,
 	surname varchar(40) not null,
 	name varchar(40) not null,
@@ -12,14 +12,14 @@ create table Performers(
 	id_style integer references Style(id)
 );
 
-create table Albom(
+CREATE TABLE Albom(
 	id serial primary key,
 	id_performer integer references Performers(id),
 	albom_name varchar(150) not null,
 	release_year date not null
 );
 
-create table music_tracks(
+CREATE TABLE Tracks(
 	id serial primary key,
 	id_albom integer references Albom(id),
 	track_name varchar(150) not null,
