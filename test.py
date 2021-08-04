2bb186c8c5c9c285e36ea7126ef507cf2668029a
Nikita <wolf24ru@gmail.com>
2021-08-04 08:50:21 +0700

    i don't knowe@

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-g = [x for x in range(10)]
-print(g)
+from DPM1Standart import DPM1Standart
+
+
+def readx(width):
+    w = width.get('бла-бла')
+    return w
+
+
+if __name__ == '__main__':
+	width = {'какая-то_херня': 123,
+	'тут_тоже': 456}
+	
+    DPM1Standart(readx(width))
