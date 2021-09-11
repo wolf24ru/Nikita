02787580a6230a7fc784fa8c18b62896944548d0
Nikita <wolf24ru@gmail.com>
2021-09-11 21:24:04 +0700

    done first task in HomeWork 5

---
+++ Module_2/HW5_Decorators/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+def log_function(old_function):
+	def new_function(*arg, **kwarg):
+		time_naw = datetime.now()
+		result = old_function(*arg, **kwarg)
+		name_function = old_function.__name__
+		file = ('log_file.txt')
+		all_information = f'- {time_naw}, {name_function}, {arg}, {kwarg}, {result}\n'
+
+		with open(file, 'a') as f:
+			f.write(all_information)
+		return result
+	
+	return new_function
+
+@log_function
+def compound_interest(percentage, years, amount):
+	total_amount = (1 + percentage / 100)**years * amount
+	return f'Итоговая ссума по вкладу {total_amount:.2f}'
+
+
+if __name__ == '__main__':
+	result = compound_interest(8, 10, 50000)
+	print(result)
