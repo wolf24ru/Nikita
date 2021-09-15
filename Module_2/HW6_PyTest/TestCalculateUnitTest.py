c8ddac42b5d165fd8c4b93f6a500e5ab8bf0915a
Nikita <wolf24ru@gmail.com>
2021-09-15 23:01:00 +0700

    update gitignore

--- Module_2/HW6_PyTest/TestCalculateUnitTest.py
+++ Module_2/HW6_PyTest/TestCalculateUnitTest.py
@@ -1,13 +1,12 @@
 # import hw5
 from hw5 import documents, directories
 from hw5 import doc_is_exist, name_form_number_doc, shelf_form_number_doc, all_document_list, add_new_document, \
-	delete_document, change_shelf, add_shelf
-
+    delete_document, change_shelf, add_shelf
 
 
 class TestCalculatePytest:
-	def test_doc_is_exist(self):
-		assert doc_is_exist(directories) == 1
+    def test_doc_is_exist(self):
+        assert doc_is_exist(directories) == 1
 
-	def test_find_person_on_number(self):
-		assert name_form_number_doc(documents, directories) == 6
+    def test_find_person_on_number(self):
+        assert name_form_number_doc(documents, directories) == 6
