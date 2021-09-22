314402679414cae4a55aa6413d0e6d223ea8b450
Nikita <wolf24ru@gmail.com>
2021-09-22 20:23:09 +0700

    add parameterized decorator

--- Module_2/HW5_Decorators/Log_Decor.py
+++ Module_2/HW5_Decorators/Log_Decor.py
@@ -2,24 +2,27 @@ from datetime import datetime
 from pathlib import Path
 
 
-def log_function(old_function):
-    def new_function(*arg, **kwarg):
-        time_naw = datetime.now()
-        result = old_function(*arg, **kwarg)
-        name_function = old_function.__name__
-        file = ('log_file.txt')
-        path_to_file = Path(Path(file).cwd(), file)
-        all_information = f'- {time_naw}, {name_function}, {arg}, {kwarg}, {result}, {path_to_file}\n'
-        with open(file, 'a') as f:
-            f.write(all_information)
-        return result
+def log_file(path_log):
+    def log_function(old_function):
+        def new_function(*arg, **kwarg):
+            time_naw = datetime.now()
+            result = old_function(*arg, **kwarg)
+            name_function = old_function.__name__
+            file = path_log
+            path_to_file = Path(Path(file).cwd(), file)
+            all_information = f'- {time_naw}, {name_function}, {arg}, {kwarg}, {result}, {path_to_file}\n'
+            with open(file, 'a') as f:
+                f.write(all_information)
+            return result
 
-    return new_function
+        return new_function
+
+    return log_function
 
 
-@log_function
+@log_file('log_file.txt')
 def compound_interest(percentage, years, amount):
-    total_amount = (1 + percentage / 100)**years * amount
+    total_amount = (1 + percentage / 100) ** years * amount
     return f'Итоговая ссума по вкладу {total_amount:.2f}'
 
 
